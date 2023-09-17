8c5896ba65273dd9507d563bdf1e42cf29fabc38
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-17 15:24:32 +0330

    refactor :: all pages are now double quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useEffect } from 'react';
-import { useReducer } from 'react';
-import Header from './Header';
-import Loader from './Loader';
-import Error from './Error';
-import Main from './Main';
-import StartScreen from './StartScreen';
+import { useEffect } from "react";
+import { useReducer } from "react";
+import Header from "./Header";
+import Loader from "./Loader";
+import Error from "./Error";
+import Main from "./Main";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
   // 'loading', 'active', 'error', 'ready', 'finished'
-  status: 'loading',
+  status: "loading",
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'dataReceived':
+    case "dataReceived":
       return {
         ...state,
         questions: action.payload,
-        status: 'ready',
+        status: "ready",
       };
 
-    case 'dataFailed':
+    case "dataFailed":
       return {
         ...state,
-        status: 'error',
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
       };
 
     default:
-      throw new Error('action is not defined !');
+      throw new Error("action is not defined !");
   }
 }
 
@@ -41,22 +48,26 @@ export default function App() {
   const numQuestions = questions.length;
 
   useEffect(() => {
-    fetch('http://localhost:8000/questions')
+    fetch("http://localhost:8000/questions")
       .then(res => res.json())
-      .then(data => dispatch({ type: 'dataReceived', payload: data }))
-      .catch(err => dispatch({ type: 'dataFailed' }));
+      .then(data => dispatch({ type: "dataReceived", payload: data }))
+      .catch(err => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
 
       <Main>
-        {status === 'loading' && <Loader />}
-        {status === 'error' && <Error />}
-        {status === 'ready' && (
-          <StartScreen numQuestions={numQuestions} />
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen
+            numQuestions={numQuestions}
+            dispatch={dispatch}
+          />
         )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
