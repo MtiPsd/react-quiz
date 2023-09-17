f552e4eb975504e9c0b923e9cba95c6d90c43406
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-17 23:36:53 +0330

    start working on timer

--- src/App.jsx
+++ src/App.jsx
@@ -128,6 +128,13 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
+
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
         )}
 
@@ -139,13 +146,6 @@ export default function App() {
             dispatch={dispatch}
           />
         )}
-
-        <NextButton
-          dispatch={dispatch}
-          answer={answer}
-          index={index}
-          numQuestions={numQuestions}
-        />
       </Main>
     </div>
   );
