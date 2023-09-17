a845492a940d448e44f472926396535e8b1d34bb
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-17 21:10:39 +0330

    Added :: functionality for points state

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const initialState = {
   status: 'loading',
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -37,9 +38,14 @@ function reducer(state, action) {
       };
 
     case 'newAnswer':
+      const question = state.questions[state.index];
       return {
         ...state,
         answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
       };
 
     default:
@@ -70,10 +76,7 @@ export default function App() {
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
         {status === 'ready' && (
-          <StartScreen
-            numQuestions={numQuestions}
-            dispatch={dispatch}
-          />
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === 'active' && (
           <Question
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,9 +1,14 @@
 function Options({ question, dispatch, answer }) {
+  //
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className="btn btn-option"
+          className={`btn btn-option ${defineStyleOnAnswer(
+            index,
+          )} ${defineStyleOnCorrectAnswer(index)} `}
           key={option}
           onClick={() =>
             dispatch({
@@ -11,12 +16,25 @@ function Options({ question, dispatch, answer }) {
               payload: index,
             })
           }
+          disabled={hasAnswered}
         >
           {option}
         </button>
       ))}
     </div>
   );
+
+  /////////////////////// helpers ///////////////////////
+
+  function defineStyleOnCorrectAnswer(index) {
+    return hasAnswered && index === question.correctOption
+      ? 'correct'
+      : 'wrong';
+  }
+
+  function defineStyleOnAnswer(index) {
+    return index === answer ? 'answer' : '';
+  }
 }
 
 export default Options;
